a4fdc7c1aadce05f94baf569d34c09191123c6da
Michael Farris <farris.michael.j@gmail.com>
2020-08-13 12:57:28 -0700

    switched to jumbotron from navbar

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Jumbotron(props) {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Employee Directory</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
